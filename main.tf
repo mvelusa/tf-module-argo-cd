038775675312c552656d25845455b972ad9bb88a
Madhu <mathukarur@gmail.com>
2025-12-24 22:28:14 -0800

    Argo CD module changes

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+resource "kubernetes_namespace_v1" "argocd-namespace" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  version    = "7.7.0"
+
+  namespace = "argocd"
+}
